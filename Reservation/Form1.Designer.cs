d2041b9e40c2352a2635568116909d566944076d
garrettfoister <garrettscottfoister@gmail.com>
2020-03-04 04:37:09 -0500

    Add project files.

---
+++ Reservation/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿using System;
+
+namespace Reservation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblArrival = new System.Windows.Forms.Label();
+            this.lblDeparture = new System.Windows.Forms.Label();
+            this.lblNights = new System.Windows.Forms.Label();
+            this.lblTotalPrice = new System.Windows.Forms.Label();
+            this.lblAvgPrice = new System.Windows.Forms.Label();
+            this.txtArrivalDate = new System.Windows.Forms.TextBox();
+            this.txtDepartureDate = new System.Windows.Forms.TextBox();
+            this.txtNights = new System.Windows.Forms.TextBox();
+            this.txtTotalPrice = new System.Windows.Forms.TextBox();
+            this.txtAvgPrice = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdoTwin = new System.Windows.Forms.RadioButton();
+            this.rdoSuite = new System.Windows.Forms.RadioButton();
+            this.rdoSingle = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblArrival
+            // 
+            this.lblArrival.AutoSize = true;
+            this.lblArrival.Location = new System.Drawing.Point(36, 38);
+            this.lblArrival.Name = "lblArrival";
+            this.lblArrival.Size = new System.Drawing.Size(86, 17);
+            this.lblArrival.TabIndex = 0;
+            this.lblArrival.Text = "Arrival Date:";
+            this.lblArrival.Click += new System.EventHandler(this.lblArrival_Click);
+            // 
+            // lblDeparture
+            // 
+            this.lblDeparture.AutoSize = true;
+            this.lblDeparture.Location = new System.Drawing.Point(36, 80);
+            this.lblDeparture.Name = "lblDeparture";
+            this.lblDeparture.Size = new System.Drawing.Size(110, 17);
+            this.lblDeparture.TabIndex = 1;
+            this.lblDeparture.Text = "Departure Date:";
+            // 
+            // lblNights
+            // 
+            this.lblNights.AutoSize = true;
+            this.lblNights.Location = new System.Drawing.Point(36, 207);
+            this.lblNights.Name = "lblNights";
+            this.lblNights.Size = new System.Drawing.Size(120, 17);
+            this.lblNights.TabIndex = 2;
+            this.lblNights.Text = "Number of nights:";
+            // 
+            // lblTotalPrice
+            // 
+            this.lblTotalPrice.AutoSize = true;
+            this.lblTotalPrice.Location = new System.Drawing.Point(36, 259);
+            this.lblTotalPrice.Name = "lblTotalPrice";
+            this.lblTotalPrice.Size = new System.Drawing.Size(79, 17);
+            this.lblTotalPrice.TabIndex = 3;
+            this.lblTotalPrice.Text = "Total price:";
+            // 
+            // lblAvgPrice
+            // 
+            this.lblAvgPrice.AutoSize = true;
+            this.lblAvgPrice.Location = new System.Drawing.Point(36, 310);
+            this.lblAvgPrice.Name = "lblAvgPrice";
+            this.lblAvgPrice.Size = new System.Drawing.Size(131, 17);
+            this.lblAvgPrice.TabIndex = 4;
+            this.lblAvgPrice.Text = "Avg. price per night";
+            // 
+            // txtArrivalDate
+            // 
+            this.txtArrivalDate.Location = new System.Drawing.Point(222, 38);
+            this.txtArrivalDate.Name = "txtArrivalDate";
+            this.txtArrivalDate.Size = new System.Drawing.Size(100, 22);
+            this.txtArrivalDate.TabIndex = 5;
+            // 
+            // txtDepartureDate
+            // 
+            this.txtDepartureDate.Location = new System.Drawing.Point(222, 80);
+            this.txtDepartureDate.Name = "txtDepartureDate";
+            this.txtDepartureDate.Size = new System.Drawing.Size(100, 22);
+            this.txtDepartureDate.TabIndex = 6;
+            // 
+            // txtNights
+            // 
+            this.txtNights.Location = new System.Drawing.Point(222, 207);
+            this.txtNights.Name = "txtNights";
+            this.txtNights.ReadOnly = true;
+            this.txtNights.Size = new System.Drawing.Size(100, 22);
+            this.txtNights.TabIndex = 7;
+            // 
+            // txtTotalPrice
+            // 
+            this.txtTotalPrice.Location = new System.Drawing.Point(222, 259);
+            this.txtTotalPrice.Name = "txtTotalPrice";
+            this.txtTotalPrice.ReadOnly = true;
+            this.txtTotalPrice.Size = new System.Drawing.Size(100, 22);
+            this.txtTotalPrice.TabIndex = 8;
+            // 
+            // txtAvgPrice
+            // 
+            this.txtAvgPrice.Location = new System.Drawing.Point(222, 310);
+            this.txtAvgPrice.Name = "txtAvgPrice";
+            this.txtAvgPrice.ReadOnly = true;
+            this.txtAvgPrice.Size = new System.Drawing.Size(100, 22);
+            this.txtAvgPrice.TabIndex = 9;
+            this.txtAvgPrice.TextChanged += new System.EventHandler(this.txtAvgPrice_TextChanged);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(78, 369);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(86, 33);
+            this.btnCalculate.TabIndex = 10;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(246, 369);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(76, 33);
+            this.btnExit.TabIndex = 11;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdoTwin);
+            this.groupBox1.Controls.Add(this.rdoSuite);
+            this.groupBox1.Controls.Add(this.rdoSingle);
+            this.groupBox1.Location = new System.Drawing.Point(33, 118);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(289, 70);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Room Type";
+            // 
+            // rdoTwin
+            // 
+            this.rdoTwin.AutoSize = true;
+            this.rdoTwin.Location = new System.Drawing.Point(105, 31);
+            this.rdoTwin.Name = "rdoTwin";
+            this.rdoTwin.Size = new System.Drawing.Size(58, 21);
+            this.rdoTwin.TabIndex = 2;
+            this.rdoTwin.Text = "Twin";
+            this.rdoTwin.UseVisualStyleBackColor = true;
+            // 
+            // rdoSuite
+            // 
+            this.rdoSuite.AutoSize = true;
+            this.rdoSuite.Location = new System.Drawing.Point(201, 31);
+            this.rdoSuite.Name = "rdoSuite";
+            this.rdoSuite.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.rdoSuite.Size = new System.Drawing.Size(61, 21);
+            this.rdoSuite.TabIndex = 1;
+            this.rdoSuite.Text = "Suite";
+            this.rdoSuite.UseVisualStyleBackColor = true;
+            // 
+            // rdoSingle
+            // 
+            this.rdoSingle.AutoSize = true;
+            this.rdoSingle.Checked = true;
+            this.rdoSingle.Location = new System.Drawing.Point(6, 31);
+            this.rdoSingle.Name = "rdoSingle";
+            this.rdoSingle.Size = new System.Drawing.Size(68, 21);
+            this.rdoSingle.TabIndex = 0;
+            this.rdoSingle.TabStop = true;
+            this.rdoSingle.Text = "Single";
+            this.rdoSingle.UseVisualStyleBackColor = true;
+            this.rdoSingle.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 425);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtAvgPrice);
+            this.Controls.Add(this.txtTotalPrice);
+            this.Controls.Add(this.txtNights);
+            this.Controls.Add(this.txtDepartureDate);
+            this.Controls.Add(this.txtArrivalDate);
+            this.Controls.Add(this.lblAvgPrice);
+            this.Controls.Add(this.lblTotalPrice);
+            this.Controls.Add(this.lblNights);
+            this.Controls.Add(this.lblDeparture);
+            this.Controls.Add(this.lblArrival);
+            this.Name = "Form1";
+            this.Text = "Reservations";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtAvgPrice_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblArrival;
+        private System.Windows.Forms.Label lblDeparture;
+        private System.Windows.Forms.Label lblNights;
+        private System.Windows.Forms.Label lblTotalPrice;
+        private System.Windows.Forms.Label lblAvgPrice;
+        private System.Windows.Forms.TextBox txtArrivalDate;
+        private System.Windows.Forms.TextBox txtDepartureDate;
+        private System.Windows.Forms.TextBox txtNights;
+        private System.Windows.Forms.TextBox txtTotalPrice;
+        private System.Windows.Forms.TextBox txtAvgPrice;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdoSingle;
+        private System.Windows.Forms.RadioButton rdoTwin;
+        private System.Windows.Forms.RadioButton rdoSuite;
+    }
+}
+
---
+++ Reservation/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reservation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            DateTime dArival = DateTime.Parse(txtArrivalDate.Text, new System.Globalization.CultureInfo("en-US"));
+            DateTime dDeparture = DateTime.Parse(txtDepartureDate.Text, new System.Globalization.CultureInfo("en-US"));
+            TimeSpan timeSpan = dDeparture - dArival;
+
+            int numOfDays = (int)timeSpan.TotalDays;
+
+            txtNights.Text = numOfDays.ToString();
+
+            double totalPrice = 0;
+
+
+
+            int txtI = 0;
+            int roomRate = 0;
+
+            // Room Price = room rate + $150 Friday or Saturday, room rate + $120 all other days
+
+
+            if (rdoSingle.Checked)
+                roomRate = 15;
+            else if (rdoTwin.Checked)
+                roomRate = 35;
+            else
+                roomRate = 90;
+
+
+            while (txtI < numOfDays)
+
+            {
+                DateTime dtTemp = dArival.AddDays(txtI);
+                DayOfWeek dowNow = dtTemp.DayOfWeek;
+                string strSWeek = dowNow.ToString();
+
+                if (strSWeek.Equals("Friday") || strSWeek.Equals("Saturday"))
+                    totalPrice = totalPrice + 150 + roomRate;
+                else
+                    totalPrice = totalPrice + 120 + roomRate;
+                txtI++;
+
+            }
+
+
+
+            txtTotalPrice.Text = totalPrice.ToString("C2", System.Globalization.CultureInfo.GetCultureInfo("en-US"));
+            double avgPrice = totalPrice / numOfDays;
+            txtAvgPrice.Text = avgPrice.ToString("C2", System.Globalization.CultureInfo.GetCultureInfo("en-US"));
+
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+
+        {
+            DateTime dArival = DateTime.Now.Date;
+            DateTime dDeparture = dArival.AddDays(3).Date;
+            txtArrivalDate.Text = dArival.ToString("MM/dd/yyyy", System.Globalization.CultureInfo.GetCultureInfo("en-US"));
+            txtDepartureDate.Text = dDeparture.ToString("MM/dd/yyyy", System.Globalization.CultureInfo.GetCultureInfo("en-US"));
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void lblArrival_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
